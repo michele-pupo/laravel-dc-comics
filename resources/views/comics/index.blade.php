a1b00f5b95e6dd2c85cf83e3636e973744353b83
michele-pupo <mikypupo@hotmail.it>
2024-04-11 19:07:59 +0200

    end exercise

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,23 +3,23 @@
 @section('content')
     
     <div class="container py-5">
-        <h1 class="text-center">Lista dei fumetti</h1>
+        <h1 class="text-center pb-4">Lista dei fumetti</h1>
         <div class="row row-cols-4 justify-content-between">
             @foreach ($comics as $comic)
-            <div class="card h-100">
+            <div class="card h-100 py-5">
                 <img src="{{$comic->thumb}}" class="card-img-top w-100 h-75 object-fit-cover" alt="{{$comic->title}}">
                 <div class="comic-body text-center">
                     <h3 class="comic-title">{{$comic->title}}</h3>
                     <h4 class="comic-series">{{$comic->series}}</h4>
                     <h5 class="comic-date text-info">{{$comic->sale_date}}</h5>
                     <span class="comic-price text-danger ">{{$comic->price}}</span><br>
-                    <a href="{{route('comics.show', $comic->id)}}">Visualizza</a>
+                    <button class="btn btn-info"><a class="text-decoration-none text-black" href="{{route('comics.show', $comic->id)}}">Visualizza</a></button>
                 </div>
             </div>
             @endforeach
         </div>
 
-        <button class="btn btn-primary"><a href="{{route('comics.create')}}">Aggiungi un fumetto</a></button>
+        <button class="btn btn-primary mt-5"><a class="text-decoration-none " href="{{route('comics.create')}}">Aggiungi un fumetto</a></button>
         
     </div>
 
