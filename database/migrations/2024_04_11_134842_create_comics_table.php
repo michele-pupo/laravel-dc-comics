72ab0248c411ccfd02b1a0ab2aba56d0d8a91647
michele-pupo <mikypupo@hotmail.it>
2024-04-11 16:53:03 +0200

    populated table with data from the db

--- database/migrations/2024_04_11_134842_create_comics_table.php
+++ database/migrations/2024_04_11_134842_create_comics_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->string('thumb');
+            $table->text('thumb');
             $table->string('price');
             $table->string('series');
             $table->date('sale_date');
             $table->string('type');
-            $table->json('artist');
-            $table->json('writers');
+            $table->text('artists');
+            $table->text('writers');
             $table->timestamps();
         });
     }
