f5f7e83bbe539d7a25cf05b0ac31805b4717e421
michele-pupo <mikypupo@hotmail.it>
2024-04-12 14:47:58 +0200

    created the edit to modify the comics

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,61 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container py-5">
-        <h1 class="text-center">Aggiungi un nuovo fumetto</h1>
-
+        <h1 class="text-center">Modifica il fumetto</h1>
 
         <form action="{{route('comics.store')}}" method="POST">
             @csrf
 
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="title" required>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="title" value="{{$comic->title}}" required>
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea type="text" class="form-control" id="exampleInputEmail1" name="description" required></textarea>
+                <textarea type="text" class="form-control" id="exampleInputEmail1" rows="8" name="description" required>{{$comic->description}}</textarea>
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Copertina</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="thumb">
+                <input type="text" class="form-control" id="exampleInputEmail1" name="thumb" value="{{$comic->thumb}}">
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="price">
+                <input type="text" class="form-control" id="exampleInputEmail1" name="price" value="{{$comic->price}}">
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="series">
+                <input type="text" class="form-control" id="exampleInputEmail1" name="series" value="{{$comic->series}}">
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data uscita</label>
-                <input type="data" class="form-control" id="exampleInputEmail1" name="sale_date">
+                <input type="data" class="form-control" id="exampleInputEmail1" name="sale_date" value="{{$comic->sale_date}}">
             </div>
 
             <div class="mb-3">
                 <label for="tipe" class="form-label">Tipo</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" name="tipe">
+                <input type="text" class="form-control" id="exampleInputEmail1" name="tipe" value="{{$comic->type}}">
             </div>
 
             <div class="mb-3">
                 <label for="artists" class="form-label">Artisti</label>
-                <textarea type="text" class="form-control" id="exampleInputEmail1" name="artists"></textarea>
+                <textarea type="text" class="form-control" id="exampleInputEmail1" name="artists">{{$comic->artists}}</textarea>
             </div>
 
             <div class="mb-3">
                 <label for="writers" class="form-label">Scrittori</label>
-                <textarea type="text" class="form-control" id="exampleInputEmail1" name="writers"></textarea>
+                <textarea type="text" class="form-control" id="exampleInputEmail1" name="writers">{{$comic->writers}}</textarea>
             </div>
 
             <button type="submit" class="btn btn-primary">Salva</button>
         </form>
     </div>
-
 @endsection
