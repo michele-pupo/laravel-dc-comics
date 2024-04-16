3d5e3068af94b85827815cad544c3c52887e5d06
michele-pupo <mikypupo@hotmail.it>
2024-04-16 15:12:11 +0200

    end exercise

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,18 +3,20 @@
 @section('content')
     
     <div class="container py-5">
-        <h1 class="text-center pb-4">Lista dei fumetti</h1>
-        <div class="row row-cols-4 justify-content-between">
+        <h1 class="text-center pb-4 display-1 fw-bold text-uppercase">Lista fumetti</h1>
+        <div class="row row-cols-6 justify-content-start row-gap-2">
             @foreach ($comics as $comic)
-            <div class="card h-100 py-5">
-                <img src="{{$comic->thumb}}" class="card-img-top w-100 h-75 object-fit-cover" alt="{{$comic->title}}">
-                <div class="comic-body text-center">
-                    <h3 class="comic-title">{{$comic->title}}</h3>
-                    <h4 class="comic-series">{{$comic->series}}</h4>
-                    <h5 class="comic-date text-info">{{$comic->sale_date}}</h5>
-                    <h6 class="comic-price text-danger pb-4">{{$comic->price}}</h6>
-                    <button class="btn btn-info"><a class="text-decoration-none text-black" href="{{route('comics.show', $comic->id)}}">Visualizza</a></button>
+            <div class="card h-auto d-flex flex-column justify-content-between p-2 border-3 border-danger">
+                <div>
+                    <img src="{{$comic->thumb}}" class="card-img-top w-100 object-fit-cover" alt="{{$comic->title}}">
+                    <div class="comic-body text-center">
+                        <h3 class="comic-title">{{$comic->title}}</h3>
+                        <h4 class="comic-series">{{$comic->series}}</h4>
+                        <h5 class="comic-date text-info">{{$comic->sale_date}}</h5>
+                        <h6 class="comic-price text-danger pb-4">{{$comic->price}}</h6>
+                    </div>
                 </div>
+                <button class="btn btn-info"><a class="text-decoration-none text-black" href="{{route('comics.show', $comic->id)}}">Visualizza</a></button>
             </div>
             @endforeach
         </div>
